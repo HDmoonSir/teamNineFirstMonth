94284edc400dd4905bc1261c149bd9ff1af3cf28
kwakchanhyuk <kwakchanhyuk@naver.com>
2023-04-13 21:21:36 +0900

    upload

---
+++ teamNineProject/static/js/script.js
@@ -0,0 +1,59 @@
+const icon_1 = document.getElementById("icon_1");
+const icon_2 = document.getElementById("icon_2");
+const icon_3 = document.getElementById("icon_3");
+const icon_4 = document.getElementById("icon_4");
+const icon_5 = document.getElementById("icon_5");
+const icon_6 = document.getElementById("icon_6");
+
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+const img5 = document.getElementById("img5");
+const img6 = document.getElementById("img6");
+
+
+icon_1.addEventListener("click", function() {
+	img1.style.display = "block";
+});
+
+icon_2.addEventListener("click", function() {
+	img2.style.display = "block";
+});
+
+icon_3.addEventListener("click", function() {
+	img3.style.display = "block";
+});
+
+icon_4.addEventListener("click", function() {
+	img4.style.display = "block";
+});
+
+icon_5.addEventListener("click", function() {
+	img5.style.display = "block";
+});
+
+icon_6.addEventListener("click", function() {
+	img6.style.display = "block";
+});
+
+
+
+img1.addEventListener("click", function() {
+	img1.style.display = "none";
+});
+img2.addEventListener("click", function() {
+	img2.style.display = "none";
+});
+img3.addEventListener("click", function() {
+	img3.style.display = "none";
+});
+img4.addEventListener("click", function() {
+	img4.style.display = "none";
+});
+img5.addEventListener("click", function() {
+	img5.style.display = "none";
+});
+img6.addEventListener("click", function() {
+	img6.style.display = "none";
+});
